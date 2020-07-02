e3e67da07e64a72ace5d3cb3a298b355a57a92f4
Eric Liu <liu.p.eric@gmail.com>
2020-07-02 00:27:42 -0700

    ch_4 project solution

---
+++ ch_4/character_picture_grid.py
@@ -0,0 +1,20 @@
+def print_picture_grid(grid):
+    row_length = len(grid[0])
+    column_length = len(grid)
+
+    for row in range(row_length):
+        for column in range(column_length):
+            print(grid[column][row], end=' ') 
+        print()
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+print_picture_grid(grid)
---
+++ ch_4/comma_code.py
@@ -0,0 +1,9 @@
+def comma_code(list):
+    combined_list = ''
+    for item in list:   
+        if item != list[len(list) - 1]:
+            combined_list += str(item) + ', '   # use str to convert all non-string elements to string type
+        else:
+            combined_list += 'and ' + str(item)     # use str to convert all non-string elements to string type
+
+    return combined_list
