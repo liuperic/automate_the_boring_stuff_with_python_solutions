d1f20d4dc6404ce12884874b056c3383d7dafb3a
Eric Liu <liu.p.eric@gmail.com>
2020-07-02 00:26:55 -0700

    ch_3 solutions

---
+++ ch_3/collatz_sequence.py
@@ -0,0 +1,12 @@
+def collatz():
+    print('Please enter an integer greater than 1 for collatz sequence')
+    number = int(input())
+    while number != 1:
+        if number % 2 == 0:
+            print(number // 2)
+            number = number // 2
+        else:
+            print(number * 3 + 1)
+            number = number * 3 + 1
+
+collatz()
